6f9fdbb495081ccaf16d863f11ecf2b4979f0f6d
vorSherer <tcpma2011@gmail.com>
2020-04-09 14:58:58 -0700

    Added minor comments, tweaks.

--- public/app.js
+++ public/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
-// alert('I live!');  // Proof of Life
+alert('I live!');  // Proof of Life
+console.log('Connected to app.js');
 
 // We can do front end JavaScript in this file
 // including jQuery and Handlebars.
-
 
--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const cors = require('cors');
 const superagent = require('superagent');
 const pg = require('pg');
 const methodOverride = require('method-override');
+
+
 const handlebars = require('handlebars');
 const template = handlebars.compile('name: {{name}}');
 console.log(template({ name: 'Nils'}));
@@ -18,16 +20,28 @@ const PORT = process.env.PORT || 3001;
 
 // middleware
 app.use(cors());                          // allows everyone to access our information
-app.use(express.static('./public'));      // serves our static files from public
-app.set('view engine', 'ejs');            // ejs template
+app.use(express.static('./public'));      // serves our static files from 'public' folder
+app.set('view engine', 'ejs');            // ejs template - searches 'views' folder first
 app.use(express.urlencoded({extended:true})); // body parser
 app.use(methodOverride('_method'));       // Allows us to convert POST or GET into PUT or DELETE
+
+
+
+
+
+
+
+
+
+
+
+
 
 // setup postgres
 const client = new pg.Client(process.env.DATABASE_URL);
 client.on('error', err => console.error(err));
 
-// Turn on server once database is connected
+// Turn on server once database is connected - ELSE Error: connect ECONNREFUSED 127.0.0.1:5432...
 client.connect()
   .then(() => {
     app.listen(PORT, () => {
